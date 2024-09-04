0f7f74165e662f533274db5ec7b3ea689e986496
Joseph Ogbu <fallyfox@gmail.com>
2024-09-04 11:45:03 +0100

    delete doc on firestore

--- app/admin/packages/page.js
+++ app/admin/packages/page.js
@@ -1,12 +1,15 @@
 "use client"
 import { useState,useEffect } from "react";
 import { db } from "@/lib/firebase.lib";
-import { onSnapshot,collection,query,orderBy,limit } from "firebase/firestore";
+import { onSnapshot,collection,query,orderBy,limit,deleteDoc,doc } from "firebase/firestore";
 import { AdminPackageCard } from "@/components/AdminPackageCard";
+import { Button,CircularProgress } from "@mui/material";
 
 export default function Packages () {
     const [packages,setPackages] = useState([]);
     const [clickedPackage,setClickedPackage] = useState(undefined);
+    const [selectedPackage,setSelectedPackage] = useState(null);
+    const [openProgress,setOpenProgress] = useState(false);
 
     useEffect(() => {
         onSnapshot(collection(db,"packages"),onSnap => {
@@ -23,7 +26,27 @@ export default function Packages () {
             //send to useState constant after full retreival
             setPackages(retreivedDocs);
         })
-    },[])
+    },[]);
+
+    useEffect(() => {
+        const filtered = packages.filter(item => item.id == clickedPackage)[0];
+        setSelectedPackage(filtered)
+    },[clickedPackage]);
+
+    //handle package delete on firestore
+    const handleDeletePackage = async () => {
+        setOpenProgress(true);
+        await deleteDoc(doc(db,"packages",clickedPackage))
+        .then(() => {
+            setClickedPackage(undefined);
+            selectedPackage(null);
+            setOpenProgress(false)
+        })
+        .catch((error) => {
+            setOpenProgress(false);
+            console.log(error);
+        })
+    }
 
     return (
         <section className="pt-[68px] grid grid-cols-3 gap-6 py-12 px-8 md:px-12 lg:px-16">
@@ -44,9 +67,44 @@ export default function Packages () {
                 })}
             </article>
 
-            <aside className="bg-gray-800 rounded-md p-6">
-                {/* when a package is clicked, the details will show here */}
-            </aside>
+            {
+                selectedPackage != null ?
+                <aside>
+                    {/* when a package is clicked, the details will show here */}
+                    <div className="flex flex-col gap-5 bg-gray-800 rounded-md p-6">
+                        <div className="grid grid-cols-2 gap-4 pb-2 border-b border-gray-600">
+                            <p className="text-gray-100">
+                                <span className="block text-xs mb-1 text-gray-300">Billing</span>
+                                <span className={`block ${selectedPackage?.data.paid === true ? "text-green-500" : "text-red-500"}`}>NGN{selectedPackage?.data.billing}</span>
+                            </p>
+                            <div className="flex justify-end">
+                                <p className="text-gray-100">
+                                    <span className="block text-xs mb-1 text-gray-300">Weight</span>
+                                    <span className="block">{selectedPackage?.data.weight}kg</span>
+                                </p>
+                            </div>
+                        </div>
+                        <p className="pb-2 border-b border-gray-600 text-gray-100">
+                            <span className="block text-xs mb-1 text-gray-300">Tracking Number</span>
+                            <span className="block">{selectedPackage?.data.packageId}</span>
+                        </p>
+                        <p className="pb-2 border-b border-gray-600 text-gray-100">
+                            <span className="block text-xs mb-1 text-gray-300">Description</span>
+                            <span className="block">{selectedPackage?.data.desc}</span>
+                        </p>
+                        <p className="pb-2 text-gray-100">
+                            <span className="block text-xs mb-1 text-gray-300">Dimensions</span>
+                            <span className="block">{selectedPackage?.data.dimension.l} x {selectedPackage?.data.dimension.b} x {selectedPackage?.data.dimension.h}cm</span>
+                        </p>
+
+                        <Button onClick={handleDeletePackage} variant="contained" color="error" size="large" className="flex items-center gap-4"> 
+                            {openProgress ? <CircularProgress style={{color:"white"}}/> : null}
+                            <span>Delete Package</span>
+                        </Button>
+                    </div>
+                </aside>
+                : null
+            }
         </section>
     )
 }
